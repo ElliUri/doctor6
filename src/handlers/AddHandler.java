d69287242f79b23e5106d83eb4ff03020b6614ae
Eldana <fineshine6718@gmail.com>
2025-12-06 17:26:05 +0600

    add page

---
+++ src/handlers/AddHandler.java
@@ -0,0 +1,11 @@
+import com.sun.net.httpserver.HttpExchange;
+import handlers.RouteHandler;
+
+import java.io.IOException;
+
+public class AddHandler implements RouteHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
